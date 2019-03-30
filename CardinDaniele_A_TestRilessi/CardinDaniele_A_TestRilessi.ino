fd837e343bff46018121cf61c37a1c8fb4d30f13
Cardin <daniele.cardin@barsanti.edu.it>
2019-03-30 10:47:57 +0100

    Aggiunto il bottone per avviare il test

--- CardinDaniele_A_TestRilessi/CardinDaniele_A_TestRilessi.ino
+++ CardinDaniele_A_TestRilessi/CardinDaniele_A_TestRilessi.ino
@@ -7,25 +7,35 @@
   int tempoFineBuzzer;
   int tempoPled = 0;
   int tempoPbuzzer = 0;
+  int pinBottone = 5;
+  int pinLed = 7;
+  int pinBuzzer = 6;
   LiquidCrystal lcd(0,1,8,9,10,11);
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16,2);
-  pinMode(5, INPUT);
-  digitalWrite(5, HIGH);
-  pinMode(7, OUTPUT);
-  pinMode(6,OUTPUT);
+  pinMode(pinBottone, INPUT);
+  digitalWrite(pinBottone, HIGH);
+  pinMode(pinLed, OUTPUT);
+  pinMode(pinBuzzer,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  lcd.setCursor(0,0);
+  lcd.print("Press the button"),
+  lcd.setCursor(0,1);
+  lcd.print("to start")
+  while (digitalRead(5) == HIGH){
+  }
+  lcd.clear();
   int tempoLed = random(1000, 7000);
   int tempoBuzzer = random(1000, 7000);
   lcd.setCursor(0,0);
   lcd.print("Per avviare il");
   lcd.setCursor(0,0);
   lcd.print("test");
-  while (digitalRead(5) == HIGH){
+  while (digitalRead(pinBottone) == HIGH){
   }
   lcd.clear();
   lcd.setCursor(0,0);
@@ -34,24 +44,24 @@ void loop() {
   lcd.print("led");
   delay(tempoLed);
   tempoPled = millis();
-  digitalWrite(7,HIGH);
-  while (digitalRead(5) == HIGH){  
+  digitalWrite(pinLed,HIGH);
+  while (digitalRead(pinBottone) == HIGH){  
   }
   lcd.clear();
   tempoPled = millis() - tempoPled;
-  digitalWrite(7,LOW);
+  digitalWrite(pinLed,LOW);
   lcd.setCursor(0,0);
   lcd.print("Be ready for the");
   lcd.setCursor(0,1);
   lcd.print("buzzer");
   delay(tempoBuzzer);
   tempoPbuzzer = millis();
-  tone(6, 1000);
-  while (digitalRead(5) == HIGH){  
+  tone(pinBuzzer, 1000);
+  while (digitalRead(pinBottone) == HIGH){  
   }
   lcd.clear();
   tempoPbuzzer = millis() - tempoPbuzzer;
-  tone(6, 1000, 1);
+  tone(pinBuzzer, 1000, 1);
   lcd.setCursor(0,0);
   lcd.print(tempoPled);
   lcd.setCursor(0,1);
@@ -76,6 +86,6 @@ void loop() {
     lcd.setCursor(7,1);
     lcd.print("bad");
   }
-  delay(10000);
+  delay(5000);
   lcd.clear();
 }
